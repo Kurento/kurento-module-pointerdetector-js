35c888f0820647f69ec88309129efce5c5c800e9
Jenkins Kurento <kurento@openvidu.io>
2022-12-29 20:45:08 +0000

    Code autogenerated from Kurento/kms-pointerdetector@ec86a01

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt)
     bower:
     {
       TOKEN:      process.env.TOKEN,
-      repository: 'Kurento/kurento-module-pointerdetector-js'
+      repository: 'https://github.com/Kurento/kurento-module-pointerdetector-js.git'
     },
 
     // Generate documentation
